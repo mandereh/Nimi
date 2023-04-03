9549cc640f5d43ab7267d71d0788d8621de60c80
Daerego Braide <phronesis4xt@gmail.com>
2023-04-03 11:16:35 +0100

    adjust

--- main.py
+++ main.py
@@ -5,56 +5,63 @@ import datetime
 import wikipedia
 import pyjokes
 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
-
-
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'anita' in command:
-                command = command.replace('anita', '')
-                print(command)
-    except:
-        pass
-    return command
-
-
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who is' in command:
-        person = command.replace('who is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
-
-
-while True:
-    run_alexa()
+class Alexa:
+    
+    def __init__(self) -> None:
+            self.listener = sr.Recognizer()
+            self.engine = pyttsx3.init()
+            self.voices = self.engine.getProperty('voices')
+            self.engine.setProperty('voice',self.voices[1].id)
+            
+            
+    def talk(self,text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+        
+        
+    def take_command(self):
+        try:
+            with sr.Microphone() as source:
+                print('listening....')
+                voice = self.listener.listen(source)
+                command = self.listener.recognize_google(voice)
+                command = command.lower()
+                if 'alexa' in command:
+                    command = command.replace('alexa','')
+                    print(command)
+        except:
+            command = ''
+        return command
+    
+    def run(self):
+        while True:
+            command = self.take_command()
+            print(command)
+            if 'play' in command:
+                song = command.replace('play','')
+                self.talk('playing'+song)
+                pywhatkit.playonyt(song)
+            elif 'time' in command:
+                time = datetime.datetime.now().strftime('%I:%M %p')
+                self.talk('current time is '+ time)
+            elif 'who is' in command:
+                person = command.replace('who is', '')
+                info = wikipedia.summary(person, 1)
+                print(info)
+                self.talk(info)
+            elif 'date' in command:
+                self.talk('sorry, I have a headache')
+            elif 'are you single' in command:
+                self.talk('I am in a relationship with wifi')
+            elif 'joke' in command:
+                self.talk(pyjokes.get_joke())
+            else:
+                self.talk('Please say the command again.')
+                
+if __name__ == "__main__":
+    alexa = Alexa()
+    alexa.run()
+            
+        
+        
+            
